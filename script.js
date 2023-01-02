37fc1c52711f7fbd10e1cd72e947e87a15f1504f
lukas noreika <lukas5@gmail.com>
2023-01-02 12:39:45 +0200

    writes temperature based on your city, writes city on the side of temperature, added icon folder with weather icons.

--- script.js
+++ script.js
@@ -3,64 +3,71 @@ const minutes = document.querySelector(".clock-minutes");
 const month = document.querySelector(".month");
 const day = document.querySelector(".day");
 const temp = document.querySelector(".temp-number");
+const city = document.querySelector(".city");
 const myKey = config.MY_KEY;
+let myCity = null;
+
+const getDate = () => {
+  let m = new Date().getMonth() + 1;
+  let currentMonth = m < 10 ? "0" + m : m;
+  let d = new Date().getDate();
+  let currentDay = d < 10 ? "0" + d : d;
+  let h = new Date().getHours();
+  let currentHours = h < 10 ? "0" + h : h;
+  let min = new Date().getMinutes();
+  let currentMin = min < 10 ? "0" + min : min;
+
+  month.innerHTML = currentMonth;
+  day.innerHTML = currentDay;
+  hours.innerHTML = currentHours;
+  minutes.innerHTML = currentMin;
+};
+
+const getCity = async () => {
+  try {
+    let res = await fetch("https://ipapi.co/json/");
+    let data = await res.json();
+    // if city name is longer then 4 letters, it will be shortened.
+    city.innerHTML =
+      data.city.length > 4 ? data.city.slice(0, 4) + "..." : data.city;
+    return data.city;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getWeather = async () => {
+  try {
+    myCity = await getCity();
+    let res = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${myKey}&q=${myCity}&aqi=no`
+    );
+    let weather = await res.json();
+    temp.innerHTML = weather.current.temp_c;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDate();
+getWeather();
 
 const loop = () => {
+  //Every second get date.
   setInterval(() => {
-    let m = new Date().getMonth() + 1;
-    let currentMonth = m < 10 ? "0" + m : m;
-    let d = new Date().getDate();
-    let currentDay = d < 10 ? "0" + d : d;
-    let h = new Date().getHours();
-    let currentHours = h < 10 ? "0" + h : h;
-    let min = new Date().getMinutes();
-    let currentMin = min < 10 ? "0" + min : min;
-
-    month.innerHTML = currentMonth;
-    day.innerHTML = currentDay;
-    hours.innerHTML = currentHours;
-    minutes.innerHTML = currentMin;
+    getDate();
   }, 1000);
 
+  //Every 30 minutes check weather.
   setInterval(() => getWeather(), 1000 * 60 * 30);
 };
 
-const getIP = () => {
-  fetch("https://ipapi.co/json/")
-    .then(function (response) {
-      response.json().then((jsonData) => {
-        console.log(jsonData);
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
-getIP();
-const getLocation = () => {
-  fetch(`https://ipapi.co/78.58.44.60/json/`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((location) => {
-      console.log(location);
-    });
-};
-// getLocation();
-
-const getWeather = () => {
-  fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${myKey}&q=klaipeda&aqi=no`
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((weather) => {
-      console.log(weather);
-      temp.innerHTML = weather.current.temp_c;
-    });
-};
-
-getWeather();
-
 loop();
+
+city.addEventListener("mouseover", () => {
+  if (myCity != null) city.innerHTML = myCity;
+});
+city.addEventListener("mouseout", () => {
+  if (myCity != null)
+    city.innerHTML = myCity.length > 4 ? myCity.slice(0, 4) + "..." : myCity;
+});
